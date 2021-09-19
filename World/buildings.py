9915f4359247813b8e3935f3f5ca4cfcbd244952
Your Name <andrewamuratov@gmail.com>
2021-09-19 14:58:55 -0400

    Converted parameter into argument

--- World/buildings.py
+++ World/buildings.py
@@ -1,6 +1,4 @@
 class Building:
-    def __init__(self, floors, corners):
+    def __init__(self, floors, *corners):
         self.floors = floors
         self.corners = corners
-        
-class School(Building)
